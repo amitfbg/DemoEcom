670aba52af76d9081891b9721d292101232e686b
Amit Kumar <amit.kumar@wingify.com>
2022-05-27 17:07:29 +0530

    Added the routes for different views with dummy response

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.scss";
+import Home from "./components/Home";
+import Products from "./components/Products";
+
 function App() {
-  return <div className="app">Demo </div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/men/products">
+            <Products />
+          </Route>
+          <Route path="/women/products">
+            <Products />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UserTypeCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserTypeCard() {
+  return <div>UserTypeCard</div>;
+}
+
+export default UserTypeCard;
